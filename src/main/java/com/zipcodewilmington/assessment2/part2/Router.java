29d5be0f6e6f935243f65f6f87e5c65a7bffc70a
Aswathy Kanakarajan <aswathykanakarajan@zipcoders-MacBook-Pro-7.local>
2019-02-27 15:11:45 -0500

    part 2 - All Passed

--- src/main/java/com/zipcodewilmington/assessment2/part2/Router.java
+++ src/main/java/com/zipcodewilmington/assessment2/part2/Router.java
@@ -1,20 +1,43 @@
 package com.zipcodewilmington.assessment2.part2;
 
+import java.lang.reflect.Array;
+import java.util.*;
+
 public class Router {
+    HashMap<String,String> map;
+
+    public Router() {
+        this.map = new HashMap<String, String>();
+    }
+
     public void add(String path, String controller) {
+        map.put(path,controller);
     }
 
     public Integer size() {
-        return null;
+        return map.size();
     }
 
     public String getController(String path) {
-        return null;
+        return map.get(path);
     }
 
     public void update(String path, String studentController) {
+        map.remove(path);
+        map.put(path, studentController);
     }
 
     public void remove(String path) {
+        map.remove(path);
+    }
+    public String toString(){
+        Set<String> keySet = map.keySet();
+        ArrayList<String> sortedList = new ArrayList<>(Arrays.asList(keySet.toArray(new String[keySet.size()])));
+        Collections.sort(sortedList);
+        String resultStr = "";
+        for(String path : sortedList){
+             resultStr = resultStr +  path + " -> " + map.get(path) + "\n";
+        }
+        return resultStr;
     }
 }
